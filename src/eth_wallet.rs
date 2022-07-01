01fe139f15fc2454fbf52adc19889d8bcdfa10f8
RexDeux <nm.cdc@hotmail.com>
2022-07-01 14:37:04 +0100

    json with wallet information

--- src/eth_wallet.rs
+++ src/eth_wallet.rs
@@ -1,10 +1,56 @@
+use anyhow::{bail, Result, Ok};
 use secp256k1::{
     rand::{rngs, SeedableRng},
     PublicKey, SecretKey,
 };
+use serde::{Deserialize, Serialize};
+use std::{io::BufWriter, ops::Add};
+use std::str::FromStr;
+use std::{fs::OpenOptions, io::BufReader};
+use tiny_keccak::keccak256;
+use web3::{types::Address, ethabi::ethereum_types::Public};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Wallet {
+    pub secret_key: String,
+    pub public_key: String,
+    pub public_address: String,
+}
 
 pub fn generate_keypair() -> (SecretKey, PublicKey) {
     let secp = secp256k1::Secp256k1::new();
     let mut rng = rngs::StdRng::seed_from_u64(111);
     secp.generate_keypair(&mut rng)
+}
+
+pub fn public_key_address(public_key: &PublicKey) -> Address {
+    let public_key = public_key.serialize_uncompressed();
+
+    debug_assert_eq!(public_key[0], 0x04);
+    let hash = keccak256(&public_key[1..]);
+
+    Address::from_slice(&hash[12..])
+}
+
+impl Wallet {
+    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
+        let addr: Address = public_key_address(&public_key);
+        Wallet {
+            secret_key: secret_key.to_string(),
+            public_key: public_key.to_string(),
+            public_address: format!("{:?}", addr),
+        }
+    }
+    
+    pub fn save_to_file(&self, file_path: &str) -> Result<()> {
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(file_path)?;
+        let buf_writer = BufWriter::new(file);
+
+        serde_json::to_writer_pretty(buf_writer, self)?;
+        
+        Ok(())
+    }
 }
